3f69c99e8e5171dd4718e79247b9e6de0a758105
phydev <mmoreirauc@gmail.com>
2020-06-02 22:41:37 +0100

    update travis

--- tests/general.js
+++ tests/general.js
@@ -1,17 +1,16 @@
 'use strict';
-
-const { Console } = require('console');
+const assert = require('assert');
 
 const anArray = require('../PhyBioJS/array_class');
 
 let phi = anArray.Zeros(10, 10);
 let psi = anArray.Random(10, 10);
 
-console.log('Travis-ci testing');
-if(phi===phi){
-    console.log('M x N zeroes array . . . ok!');
-}
-
-if(psi===psi){
-    console.log('M x N random array . . . ok!');
-}
+describe('Array definition tests', () => {
+    it('should return true', () => {
+        assert.equal(phi, phi);
+    });
+    it('should return true', () => {
+        assert.equal(psi, psi);
+    });
+});
