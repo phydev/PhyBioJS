66fcd3c710df5b2f42154965558ad5b0d4456065
phydev <mmoreirauc@gmail.com>
2020-06-02 20:44:37 +0100

    testing

--- tests/general.js
+++ tests/general.js
@@ -1,14 +1,15 @@
-const anArray = require('../PhyBioJS/phybio.js');
+'use strict';
 
+const anArray = require('../PhyBioJS/phybio.js');
 
 let phi = anArray.Zeros(10, 10);
 let psi = anArray.Random(10, 10);
 
 console.log("Travis-ci testing");
 if(phi===phi){
-    console.log("M x N zeroes array . . . ok!")
+    console.log("M x N zeroes array . . . ok!");
 }
 
 if(psi===psi){
-    console.log("M x N random array . . . ok!")
+    console.log("M x N random array . . . ok!");
 }
