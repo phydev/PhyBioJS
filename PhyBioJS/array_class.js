4f8be112e2e8fab205e1793f46e77732e7f9320b
phydev <mmoreirauc@gmail.com>
2020-06-03 17:43:38 +0100

    droplet model

--- PhyBioJS/array_class.js
+++ PhyBioJS/array_class.js
@@ -28,6 +28,30 @@ class anArray {
         return newArray;
 
     }
+
+    static Circle(m, n, radius){
+
+        let newArray = [[]];
+        let r;
+
+        for (var i = 0; i < m; i++) {
+           for (var j = 0; j < n; j++) {
+               r = Math.sqrt(Math.pow(m/2-i, 2) + Math.pow(n/2-j, 2));
+
+               if (r <= radius) {
+                   newArray[i].push(1.0);
+               }
+               else{
+                   newArray[i].push(0.0);
+               }
+
+           }
+           newArray.push([]);
+        }
+    return newArray;
+
+    }
 }
 
-module.exports = anArray;
+if(typeof process === 'object')
+    module.exports = anArray;
--- PhyBioJS/phybio.js
+++ PhyBioJS/phybio.js
@@ -1,8 +1,16 @@
 'use strict';
 
-function runSim(model, tsteps, Lx, Ly){
+function runSim(model, tsteps, Lx, Ly, cell_radius){
 
-    let phi = anArray.Random(Lx, Ly);
+    let phi;
+
+    if (model=='droplet();'){
+        phi = anArray.Circle(Lx, Ly, cell_radius);
+    }
+    else{
+        phi = anArray.Random(Lx, Ly);
+    }
+
     const dt = 0.001;
     const outPeriod = 1000;
     var layout = {
@@ -33,13 +41,13 @@ function runSim(model, tsteps, Lx, Ly){
     Plotly.newPlot('plotDiv', data, layout);
 
 
-    integrate(model, phi, tsteps, outPeriod, dt, Lx, Ly, layout);
+    integrate(model, phi, tsteps, outPeriod, dt, Lx, Ly, layout, cell_radius);
 
 }
 
-function integrate(model, phi, tsteps, outPeriod, dt, Lx, Ly, layout){
+function integrate(model, phi, tsteps, outPeriod, dt, Lx, Ly, layout, cell_radius){
     var nsteps = 0;
-
+    var iPersistency = 0.0;
 
     function allen_cahn(){
 
@@ -98,6 +106,47 @@ function integrate(model, phi, tsteps, outPeriod, dt, Lx, Ly, layout){
         }
     }
 
+
+    function droplet(){
+
+        let phi_o = anArray.Zeros(Lx, Ly);
+        var volume;
+        var niter = outPeriod;
+        var persistency_time = 10000;
+        var chi = 10;
+        let noise = [0.0, 0.0];
+        let grad = [0.0, 0.0];
+        const volume_target = Math.PI*Math.pow(cell_radius,2);
+
+        while (niter-- && nsteps<=tsteps ) {
+            //for (var nstep = 0; nstep <= tsteps; nstep++) {
+            phi_o = phi.slice();
+            volume = sum(phi, Lx, Ly);
+
+            if( iPersistency >= persistency_time){
+                noise = [1-2*Math.random(), 1-2*Math.random()];
+                iPersistency =0.0;
+            }
+            for (var i = 0; i < Lx; i++) {
+                for (var j = 0; j < Ly; j++) {
+                    grad = gradient(phi_o, i, j, Lx, Ly);
+                    phi[i][j] = phi_o[i][j] + dt * (
+                        -chi*(grad[0]*noise[0] + grad[1]*noise[1])
+                        +laplacian(phi_o, i, j, Lx, Ly)
+                        + phi_o[i][j] * (1.0 - phi_o[i][j]) * (phi_o[i][j] - 0.5 + (volume_target-volume)));
+                }
+            }
+            nsteps++;
+            iPersistency++;
+        }
+        if (nsteps <= tsteps) {
+            console.log(nsteps);
+            Plotly.animate('plotDiv',{
+                data:{z: phi},
+                type:'heatmap', colorscale: 'Electric', zsmooth: 'best'}, layout);
+            setTimeout(droplet, 1);
+        }
+    }
     eval(model);
     //cahn_hilliard();
     //allen_cahn();
@@ -136,3 +185,36 @@ function b(x, L){
     }
     return x;
 }
+
+function sum(phi, Lx, Ly){
+    let _sum = 0.0;
+
+    for (var i = 0; i < Lx; i++) {
+        for (var j = 0; j < Ly; j++) {
+            _sum += phi[i][j];
+        }
+
+    }
+    return _sum;
+}
+
+function gradient(fn, x1, x2, Lx, Ly){
+    // compute the laplacian of fn at the point (x1, x2)
+    var yh = 0.0;
+    var yl = 0.0;
+    let s = [x1, x2];
+    let gradient_value = [0.0, 0.0];
+
+    for (var k = 0; k <= 1; k++) {
+        s[k] += 1;
+        yh = fn[b(s[0], Lx)][b(s[1], Ly)];
+        s[k] -= 2;
+        yl = fn[b(s[0], Lx)][b(s[1], Ly)];
+        s[k] +=1;
+        gradient_value[k] = (yh - yl)/2.0;
+    }
+
+    return gradient_value;
+
+}
+
